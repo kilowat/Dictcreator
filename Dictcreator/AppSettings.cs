79d5ad2411338dee17fafdfc420ab8f50639272b
kilowat <kadantsev.sergey@gmail.com>
2019-08-21 17:39:10 +0300

    Work with ui handlers

---
+++ Dictcreator/AppSettings.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictcreator
+{
+    public class AppSettings
+    {
+        #region Properties
+        private static AppSettings _instance;
+
+        private Dictionary<string, int> _colCharIndexMap = new Dictionary<string, int>();
+
+        private string _fileXlsPath = string.Empty;
+        private string _colNumberIndex = string.Empty;
+        private string _colEnWord = string.Empty;
+        private string _colTranscript = string.Empty;
+        private string _colTranslation = string.Empty;
+        private string _colExamples = string.Empty;
+        private int _maxExample = 0;
+        private string _colAudio = string.Empty;
+        private string _colYouglish = string.Empty;
+        private string _colContextReverso = string.Empty;
+        private string _colWoordHunt = string.Empty;
+        private string _colMerWebster = string.Empty;
+        private int _startNumberIndex = 1;
+
+        public static AppSettings Instance
+        {
+            private set { }
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new AppSettings();
+                }
+                return _instance;
+            }
+        }
+
+        public string FileXlsPath { get => _fileXlsPath; set => _fileXlsPath = value; }
+        public string ColNumberIndex { get => _colNumberIndex; set => _colNumberIndex = value; }
+        public string ColEnWord { get => _colEnWord; set => _colEnWord = value; }
+        public string ColTranscript { get => _colTranscript; set => _colTranscript = value; }
+        public string ColTranslation { get => _colTranslation; set => _colTranslation = value; }
+        public string ColExamples { get => _colExamples; set => _colExamples = value; }
+        public int MaxExample { get => _maxExample; set => _maxExample = value; }
+        public string ColAudio { get => _colAudio; set => _colAudio = value; }
+        public string ColYouglish { get => _colYouglish; set => _colYouglish = value; }
+        public string ColContextReverso { get => _colContextReverso; set => _colContextReverso = value; }
+        public string ColWoordHunt { get => _colWoordHunt; set => _colWoordHunt = value; }
+        public string ColMerWebster { get => _colMerWebster; set => _colMerWebster = value; }
+        public int StartNumberIndex
+        {
+            get => _startNumberIndex; 
+            set
+            {
+                if (value < 1)
+                {
+                    value = 1;
+                }
+                _startNumberIndex = value;
+            }
+        }
+
+        #endregion
+
+        private AppSettings()
+        {
+            InitColCharIndexMap();
+        }
+
+        private void InitColCharIndexMap()
+        {
+            int i = 1;
+            for (char letter = 'A'; letter <= 'Z'; letter++)
+            {
+                _colCharIndexMap[letter.ToString()] = i;
+                i++;
+            }
+        }
+    }
+}
--- Dictcreator/MainWindow.xaml.cs
+++ Dictcreator/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,36 @@ namespace Dictcreator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private OpenFileDialog _openFielDialog;
         public MainWindow()
         {
             InitializeComponent();
+            InitApp();
+        }
+
+        private void ClickStartButton(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ClickCancelButton(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ClickSelectTableButton(object sender, RoutedEventArgs e)
+        {
+            if (_openFielDialog.ShowDialog() == true)
+            {
+                var arrPath = _openFielDialog.FileName.Split('\\');
+                selectFielTextBox.Text = arrPath.Last();
+            }
+        }
+
+        private void InitApp()
+        {
+            _openFielDialog = new OpenFileDialog();
+            _openFielDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
         }
     }
 }
